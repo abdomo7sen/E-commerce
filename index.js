cb291d07545bd8a9d49f207843f3ea16189f4e75
abdomo7sen <abdomohsen20006@gmail.com>
2024-09-25 23:04:44 +0300

    update

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ process.on("uncaughtException",(err)=>{
     console.log(err);
 })
 import express from 'express'
-// import * as dotenv from "dotenv"
+import * as dotenv from "dotenv"
 import { conn } from './database/dbConnection.js'
 import { globalError } from './src/middleware/globalError.js'
 import { AppError } from './src/utils/appError.js';
 import { bootstrape } from './src/modules/bootstrape.js';
 import cors from "cors"
-// dotenv.config({path:'./config/.env'})
+dotenv.config({path:'./config/.env'})
 const app = express()
 const port =process.env.PORT||3000
 app.use(cors())
@@ -16,7 +16,6 @@ app.use(express.json())
 app.use('/uploads',express.static('uploads'))
 
 bootstrape(app)
-
 app.use("/*",( req, res,next) => {
     next(new AppError(`Route Not Found ${req.originalUrl}`,404))
 })
